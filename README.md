e644006dfb0c67b3fce18f1e2a9b020524cf81bd
Gabriel <gabriel@gabstep.com.br>
2024-06-25 21:32:36 -0300

    Updates: Match new version (2024-06-25) + Added XL mode for every server + Update player counter every minute

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ It has only static files, generally it will not cause problems in hosting.
 
 ## How to use
 
-Open the <a href="br.html">`br.html`</a> file in your browser to play in the Brazilian server.
+Open the <a href="br-xl.html">`br-xl.html`</a> or <a href="br.html">`br.html`</a> file in your browser to play in the Brazilian server.
 
-Open the <a href="us.html">`us.html`</a> file in your browser to play in the English server.
+Open the <a href="us-xl.html">`us-xl.html`</a> or <a href="us.html">`us.html`</a> file in your browser to play in the English server.
 
-Open the <a href="es.html">`es.html`</a> file in your browser to play in the Spanish server.
+Open the <a href="es-xl.html">`es-xl.html`</a> or <a href="es.html">`es.html`</a> file in your browser to play in the Spanish server.
 
-Open the <a href="dev.html">`dev.html`</a> file in your browser to play in the development server.
+Open the <a href="dev-xl.html">`dev-xl.html`</a> or <a href="dev.html">`dev.html`</a> file in your browser to play in the development server.
 
 ## How to update
 
---
+++ scripts/online.js
@@ -0,0 +1,40 @@
+
+
+function updateOnlineUsers(locale) {
+    var urls = {
+        br: "https://origins.habbo.com.br/api/public/origins/users",
+        us: "https://origins.habbo.com/api/public/origins/users",
+        es: "https://origins.habbo.es/api/public/origins/users",
+        dev: null
+    };
+    function fetchOnlineUsers(callback) {
+        var url = urls[locale];
+        if(!url) {
+            callback("Failed to fetch online users");
+            return;
+        }
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4) { // request finished and response is ready
+                if(this.status == 200) {
+                    var data = JSON.parse(this.responseText);
+                    if(data.onlineUsers) {
+                        callback(data.onlineUsers+" members online");
+                        return;
+                    }
+                }
+                callback("Failed to fetch online users");
+            }
+        };
+        xhttp.open("GET", url, true);
+        xhttp.send();
+    }
+    function updateOnlineUsers() {
+        fetchOnlineUsers(function(onlineMessage) {
+            document.getElementById("habboCountUpdateTarget").innerHTML = onlineMessage;
+        });
+    }
+
+    updateOnlineUsers();
+    setInterval(updateOnlineUsers, 60000); // update every minute
+}
